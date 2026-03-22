32d8859e2943dacdcd536aee9e5103cb307df07a
waimea-rastratford <rastratford@waimea.school.nz>
2026-03-23 10:59:55 +1300

    Board is complete, 4 counters placed
    Created the Spaces, placed the 4 black counters, working on the 5th white counter

--- src/Main.kt
+++ src/Main.kt
@@ -12,8 +12,86 @@
  */
 
 
+val board = mutableListOf<String>()
+var boardSize = 16
+val takenSpaces = mutableListOf<Int>()
 
 fun main() {
-    println("Hello World!")
+
+    createBoard()
+    addCounters()
+    showBoard()
+
+
+
 }
+
+
+fun createBoard() {
+
+    while (board.size < boardSize) {
+        board.add("-")
+    }
+
+
+}
+
+fun addCounters() {
+    val blackCounter = "0"
+
+    for (i in 0..4) {
+
+        val random = 1..boardSize
+        val randomSpace = random.random()
+        board[randomSpace] = blackCounter
+        takenSpaces.add(randomSpace)
+
+    }
+
+    val random = 1..boardSize
+    if (random in takenSpaces) {
+        
+    }
+
+
+/*
+    val randomSpace = Int
+
+    while (randomSpace in takenSpaces) {
+        val random = 1..boardSize
+
+    }
+*/
+
+    val whiteCounter = "X"
+
+
+
+
+}
+
+fun showBoard() {
+
+    print("┌")
+    print("───┬".repeat(board.size - 1))
+    println("───┐")
+
+    for (space in board) {
+
+        print("│ $space ".padEnd(4))
+
+    }
+    println("│")
+    print("└")
+    print("───┴".repeat(board.size - 1))
+    println("───┘")
+    println()
+
+
+
+}
+
+
+
+
 
